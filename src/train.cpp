bf62715b1ec8cd349d5a2a4ec0794695bde90743
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-05-17 14:01:09 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,51 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+#include <random>
+
+Train::Train() : countOp(0), first(nullptr) {}
+
+Train::~Train() {
+    Car* current = first;
+    if (current == nullptr) return; // Empty train
+
+    Car* nextCar;
+    do {
+        nextCar = current->next;
+        delete current;
+        current = nextCar;
+    } while (current != first); // Iterate until we loop back to the first car
+}
+
+void Train::addCar(bool light) {
+  Car *newCar = new Car;
+  newCar->light = light;
+
+  if (first == nullptr) {
+    first = newCar;
+    newCar->next = newCar;
+    newCar->prev = newCar;
+  } else {
+    Car *last = first->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
+  }
+}
+
+int Train::getLength() {
+  if (first == nullptr) return 0;
+
+  int length = 0;
+  Car *current = first;
+  do {
+    length++;
+    current = current->next;
+    countOp++; // Count the move
+  } while (current != first);
+
+  return length;
+}
+
+int Train::getOpCount() { return countOp; }
