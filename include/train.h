cdd8ecbb1d73adcf38cecb835e7f40108a145bfc
nonpluss3d <al.kistanoff2016@yandex.ru>
2025-05-18 16:13:25 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -2,6 +2,8 @@
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
+#include <iostream>
+
 class Train {
  private:
   struct Car {
@@ -10,11 +12,14 @@ class Train {
     Car *prev;
   };
   int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
+  Car *first;  // точка входа в поезд (первый вагон)
+
  public:
   Train();
+  ~Train();
   void addCar(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
 };
+
 #endif  // INCLUDE_TRAIN_H_
